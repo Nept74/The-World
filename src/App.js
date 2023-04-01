e25a8c10ac330b08b7777ad8dc5d05f5faa85131
Mihail <apanasov.m@yandex.ru>
2023-04-01 13:12:59 +0300

    country card

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 // import './App.css';
+import CountryCard from "./components/CountryCard";
 
 function App() {
   return (
     <div className="App">
-      Hello
+      <CountryCard
+        name={'India'}
+        capital={'New Delhi'}
+        population={1415000000}
+        flagUrl ={'https://upload.wikimedia.org/wikipedia/en/thumb/4/41/Flag_of_India.svg/1200px-Flag_of_India.svg.png'}
+      />
+
+      <CountryCard
+        name={'Russia'}
+        capital={'Moskow'}
+        population={147000000}
+        flagUrl ={'https://upload.wikimedia.org/wikipedia/en/thumb/f/f3/Flag_of_Russia.svg/640px-Flag_of_Russia.svg.png'}
+      />
     </div>
   );
 }
